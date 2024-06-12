586c7f1e901d283d69b2858be5da729223dd11bd
A-mont <a.mont.24.777@gmail.com>
2024-06-12 17:37:53 -0600

    Updatre

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -2,90 +2,230 @@
 
 use gmeta::{In, InOut, Metadata, Out};
 use gstd::{prelude::*, ActorId};
+use primitive_types::U256;
 
-pub struct ProgramMetadata;
+pub struct EscrowMetadata;
 
-// 1. Define actions, events, errors and state for your metadata.
-impl Metadata for ProgramMetadata {
-    type Init = In<InitStruct>;
-    type Handle = InOut<Actions, Result<Events, Errors>>;
+impl Metadata for EscrowMetadata {
+    type Init = In<InitEscrow>;
+    type Handle = InOut<EscrowAction, EscrowEvent>;
     type Others = ();
     type Reply = ();
     type Signal = ();
-    type State = Out<IoCustomStruct>;
+    type State = Out<EscrowState>;
 }
 
-// 2. Create your init Struct(Optional)
+#[derive(Default, Encode, Decode, Clone, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct EscrowState {
+    pub ft_program_id: ActorId,
+    pub wallets: Vec<(WalletId, Wallet)>,
+    pub id_nonce: WalletId,
+    pub transaction_id: u64,
+    pub transactions: Vec<(u64, Option<EscrowAction>)>,
+}
+
+/// An escrow wallet ID.
+pub type WalletId = U256;
+
+/// Initializes an escrow program.
 #[derive(Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub struct InitStruct {
-    // Example:
+pub struct InitEscrow {
+    /// Address of a fungible token program.
     pub ft_program_id: ActorId,
 }
 
-// 3. Create your own Actions
-#[derive(Debug, Decode, Encode, TypeInfo)]
+/// An enum to send the program info about what it should do.
+///
+/// After a successful processing of this enum, the program replies with [`EscrowEvent`].
+#[derive(Clone, Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub enum Actions {
-    // Add Actions
-    FirstAction,               // Example an action with a simple input
-    SecondAction(String),      // Example an action with String input
-    ThirdAction(u128),         // Example an action with a u128 input
-    Fourthaction(CustomInput), // Example an action with a custom input
-    Fifthaction {
-        // Example an action with a custom input
-        first_field: u128,
-        second_field: Vec<ActorId>,
+pub enum EscrowAction {
+    /// Creates one escrow wallet and replies with its ID.
+    ///
+    /// # Requirements
+    /// * [`msg::source()`](gstd::msg::source) must be `buyer` or `seller` for this wallet.
+    /// * `buyer` or `seller` mustn't have the zero address.
+    ///
+    /// On success, returns [`EscrowEvent::Created`].
+    Create {
+        /// A buyer.
+        buyer: ActorId,
+        /// A seller.
+        seller: ActorId,
+        /// An amount of tokens.
+        amount: u128,
     },
+
+    /// Makes a deposit from a buyer to an escrow wallet
+    /// and changes wallet's [`WalletState`] to [`AwaitingConfirmation`](WalletState::AwaitingConfirmation).
+    ///
+    /// Transfers tokens to an escrow wallet until a deal is confirmed (by [`EscrowAction::Confirm`]) or cancelled ([`EscrowAction::Cancel`]).
+    ///
+    /// # Requirements
+    /// * [`msg::source()`](gstd::msg::source) must be a buyer for this wallet.
+    /// * Wallet mustn't be paid or closed (that is, wallet's [`WalletState`] must be [`AwaitingDeposit`](WalletState::AwaitingDeposit)).
+    ///
+    /// On success, returns [`EscrowEvent::Deposited`].
+    Deposit(
+        /// A wallet ID.
+        WalletId,
+    ),
+
+    /// Confirms a deal by transferring tokens from an escrow wallet
+    /// to a seller and changing wallet's [`WalletState`] to [`Closed`](WalletState::Closed).
+    ///
+    /// Transfers tokens from an escrow wallet to a seller for this wallet.
+    ///
+    /// # Requirements
+    /// * [`msg::source()`](gstd::msg::source) must be a buyer for this wallet.
+    /// * Wallet must be paid and unclosed (that is, wallet's [`WalletState`] must be [`AwaitingDeposit`](WalletState::AwaitingConfirmation)).
+    ///
+    /// On success, returns [`EscrowEvent::Confirmed`].
+    Confirm(
+        /// A wallet ID.
+        WalletId,
+    ),
+
+    /// Refunds tokens from an escrow wallet to a buyer
+    /// and changes wallet's [`WalletState`] back to [`AwaitingDeposit`](WalletState::AwaitingDeposit)
+    /// (that is, a wallet can be reused).
+    ///
+    /// Refunds tokens from an escrow wallet to a buyer for this wallet.
+    ///
+    /// # Requirements
+    /// * [`msg::source()`](gstd::msg::source) must be a seller for this wallet.
+    /// * Wallet must be paid and unclosed (that is, wallet's [`WalletState`] must be [`AwaitingDeposit`](WalletState::AwaitingConfirmation)).
+    ///
+    /// On success, returns [`EscrowEvent::Refunded`].
+    Refund(
+        /// A wallet ID.
+        WalletId,
+    ),
+
+    /// Cancels a deal and closes an escrow wallet by changing its [`WalletState`] to [`Closed`](WalletState::Closed).
+    ///
+    /// # Requirements
+    /// * [`msg::source()`](gstd::msg::source) must be a buyer or seller for this wallet.
+    /// * Wallet mustn't be paid or closed (that is, wallet's [`WalletState`] must be [`AwaitingDeposit`](WalletState::AwaitingDeposit)).
+    ///
+    /// On success, returns [`EscrowEvent::Cancelled`].
+    Cancel(
+        /// A wallet ID.
+        WalletId,
+    ),
+
+    /// Continues the transaction if it fails due to lack of gas
+    /// or due to an error in the token contract.
+    ///
+    /// # Requirements:
+    /// * `transaction_id` should exists in `transactions` table;
+    ///
+    /// When transaction already processed replies with [`EscrowEvent::TransactionProcessed`].
+    Continue(
+        /// Identifier of suspended transaction.
+        u64,
+    ),
 }
 
-// Example of a custom input for an action
-#[derive(Debug, Decode, Encode,  Clone, TypeInfo)]
+/// An enum that contains a result of processed [`EscrowAction`].
+#[derive(Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub struct CustomInput {
-   pub firstfield: String,
-   pub secondfield: u128,
-   pub thirdfield: ActorId,
+pub enum EscrowEvent {
+    Cancelled(
+        /// An ID of a wallet with a cancelled deal.
+        WalletId,
+    ),
+    Refunded(
+        /// Transaction id.
+        u64,
+        /// An ID of a refunded wallet.
+        WalletId,
+    ),
+    Confirmed(
+        /// Transaction id.
+        u64,
+        /// An ID of a wallet with a confirmed deal.
+        WalletId,
+    ),
+    Deposited(
+        /// Transaction id.
+        u64,
+        /// An ID of a deposited wallet.
+        WalletId,
+    ),
+    Created(
+        /// An ID of a created wallet.
+        WalletId,
+    ),
+    TransactionProcessed,
+    TransactionFailed,
 }
 
-// 4. Create your own Events
+/// Escrow wallet information.
+#[derive(Decode, Encode, TypeInfo, Clone, Copy, Debug, PartialEq, Eq)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct Wallet {
+    /// A buyer.
+    pub buyer: ActorId,
+    /// A seller.
+    pub seller: ActorId,
+    /// A wallet state.
+    pub state: WalletState,
+    /// An amount of tokens that a wallet can have. **Not** a current amount on a wallet balance!
+    pub amount: u128,
+}
+
+/// An escrow wallet state.
+#[derive(Decode, Encode, TypeInfo, PartialEq, Eq, Clone, Copy, Debug)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum WalletState {
+    AwaitingDeposit,
+    AwaitingConfirmation,
+    Closed,
+}
+
 #[derive(Debug, Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub enum Events {
-    // Add Events(Example)
-    FirstEvent,          // Example an event with a simple input
-    SecondEvent(String), // Example an event with a u128 input
-    ThirdEvent(u128),    // Example an event with String input
-    FourtEvent {
-        first_field: ActorId,
-        second_field: Vec<ActorId>, // Example an event with a custom input
+pub enum FTAction {
+    Mint(u128),
+    Burn(u128),
+    Transfer {
+        from: ActorId,
+        to: ActorId,
+        amount: u128,
     },
+    Approve {
+        to: ActorId,
+        amount: u128,
+    },
+    TotalSupply,
+    BalanceOf(ActorId),
 }
 
-// 5. Create your own Errors
-#[derive(Debug, Decode, Encode, TypeInfo)]
+#[derive(Debug, Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub enum Errors {
-    // Add errors(Example)
-    FirstError,
-    SecondError,
-    ThirdErrors,
-    FourtErrors,
-}
-
-// 6. Create your own Struct
-#[derive(Debug, Decode, Encode, TypeInfo)]
-#[codec(crate = gstd::codec)]
-#[scale_info(crate = gstd::scale_info)]
-pub struct IoCustomStruct {
-    pub firstfield: String,
-    pub secondfield: String,
-    pub thirdfield: u128,
-    pub fourthfield: Vec<(ActorId, CustomInput)> ,
-    pub fifthfield: Vec<(ActorId, u128)> ,
+pub enum FTEvent {
+    Transfer {
+        from: ActorId,
+        to: ActorId,
+        amount: u128,
+    },
+    Approve {
+        from: ActorId,
+        to: ActorId,
+        amount: u128,
+    },
+    TotalSupply(u128),
+    Balance(u128),
+    Ok
 }
